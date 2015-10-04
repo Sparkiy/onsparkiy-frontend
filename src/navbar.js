207bc8b2ba0ebff7deb119ef2b740083d992602a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2015-10-04 22:24:43 +0200

    Cleared

--- src/navbar.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Aleksandar Toplek on 23.6.2015..
- */
-
-import {bindable} from 'aurelia-framework';
-
-export class NavBar {
-  @bindable router = null;
-}
--- src/settings.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Aleksandar Toplek on 23.6.2015..
- */
-
-export class Settings {
-
-}
